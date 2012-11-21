636308c4352cb8c43488500a6ed54a27d0cc83ef
Calvin Cheng <linchuan.cheng@gmail.com>
2012-11-21 16:50:15 +0800

    Add in the scroller

--- notification/static/js/notifications.js
+++ notification/static/js/notifications.js
@@ -1,4 +1,51 @@
 $(document).ready(function() {
+
+	left_arrow = $(".notification-wrapper .container").find("a").first()
+	right_arrow = $(".notification-wrapper .container").find("a").last()
+
+	function show_next(notice_list) {
+		var visible_li = notice_list.children().filter(':visible')[0];
+		
+		next_length = $(visible_li).next('li').length
+		previous_length = $(visible_li).first().length
+
+		// determine who to show
+		if ( next_length > 0 ) {
+			next_visible_li = $(visible_li).next('li')
+		} else if ( next_length == 0 && previous_length == 1 ) {
+			next_visible_li = notice_list.children().first();
+		}
+
+		// show the next li element; and hide the current li element
+		$(next_visible_li).css("display", "block");
+		$(visible_li).css("display", "none");
+
+		return $(next_visible_li);
+	}
+
+	// render arrow icons as needed
+	$(".notification-wrapper .container").hover(
+		function () {
+			$(left_arrow).addClass("leftArrow");
+			$(right_arrow).addClass("rightArrow");
+		},
+		function () {
+			$(left_arrow).removeClass("leftArrow");
+			$(right_arrow).removeClass("rightArrow");
+		}
+	);
+
+	// notice manual rotation logic
+	var notice_list = $("ul#notifications");
+	$(left_arrow).click(function() {
+		show_next(notice_list);
+	});
+	
+	$(right_arrow).click(function() {
+		show_next(notice_list);
+	});
+
+	// notice handling logic
 	$("#notification-close").click(function() {
 		visible_li = $("ul#notifications").children().filter(':visible')[0];
 		// mark the notice object as read
